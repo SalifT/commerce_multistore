4c270384fb809a5409da4ae2a10a7780a82acb99
drugan <proshins@gmail.com>
2017-09-18 18:15:23 +0300

    Add VBO Mark as default store action

---
+++ src/Plugin/Action/MultistoreMarkAsDefault.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\commerce_multistore\Plugin\Action;
+
+use Drupal\Core\Action\ActionBase;
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * Marks store as default.
+ *
+ * @Action(
+ *   id = "commerce_multistore_mark_as_default",
+ *   label = @Translation("Mark as default store"),
+ *   type = "commerce_store"
+ * )
+ */
+class MultistoreMarkAsDefault extends ActionBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute($entity = NULL) {
+    /** @var \Drupal\commerce_store\Entity\StoreInterface $entity */
+    /** @var \Drupal\commerce_multistore\StoreStorageInterface $store_storage */
+    $store_storage = \Drupal::entityTypeManager()->getStorage('commerce_store');
+    $store_storage->markAsDefault($entity);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
+    /** @var \Drupal\commerce_store\Entity\StoreInterface $object */
+    $result = $object->access('update', $account, TRUE)
+      ->andIf($object->access('edit', $account, TRUE));
+
+    return $return_as_object ? $result : $result->isAllowed();
+  }
+
+}
