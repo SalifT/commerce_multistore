25bd4749acee12f914c50c5a0ad0940f6daa97ba
drugan <proshins@gmail.com>
2017-09-19 12:57:07 +0300

    Extend description for the default store checkbox on a store form

--- src/Form/MultistoreForm.php
+++ src/Form/MultistoreForm.php
@@ -33,13 +33,15 @@ class MultistoreForm extends StoreForm {
       }
     }
 
+    $form['default']['#description'] = $this->t('The basic role of default store is to be the last in a chain of stores resolved for a particular commerce action. For example, you may have the same product added to different stores and sold in some countries with different taxes applied. So, if no one country condition is met then this product will be handled as if it belongs to the current default store.');
+
     if ($user->hasPermission($this->getEntity()->getEntityType()->getAdminPermission())) {
       $form['default']['#title'] = $this->t('Global default store');
-      $form['default']['#description'] = $this->t("As admin you may assign for this purpose your own store or any other owner's store. Note that disregarding of this setting each regular store owner have their own default store.");
+      $form['default']['#description'] .= ' ' . $this->t("As admin you may assign for this purpose your own store or any other owner's store. Note that disregarding of this setting each regular store owner have their own default store.");
     }
     else {
       $form['default']['#title'] = $this->t('Default store');
-      $form['default']['#description'] = $this->t('Only one store might be set as default per a store owner. You can change the default store by assigning it to any other of your stores.');
+      $form['default']['#description'] .= ' ' . $this->t('Only one store might be set as default per a store owner. You can change the default store by assigning it to any other of your stores.');
     }
 
     $form['default']['#weight'] = $form['name']['#weight'] - 1;
