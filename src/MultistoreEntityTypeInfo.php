6e15c1a543fc56325e34fd57a7b4fbd7d581e6b2
drugan <proshins@gmail.com>
2017-10-02 17:36:26 +0300

    Added Products operation link for a store entity

---
+++ src/MultistoreEntityTypeInfo.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Drupal\commerce_multistore;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\commerce_store\Entity\StoreInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+
+/**
+ * Manipulates entity type information.
+ *
+ * This class contains primarily bridged hooks for compile-time or
+ * cache-clear-time hooks. Runtime hooks should be placed in EntityOperations.
+ */
+class MultistoreEntityTypeInfo implements ContainerInjectionInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
+   * EntityTypeInfo constructor.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $current_user
+   *   Current user.
+   */
+  public function __construct(AccountInterface $current_user) {
+    $this->currentUser = $current_user;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('current_user')
+    );
+  }
+
+  /**
+   * Adds multistore operations on entity that supports it.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity on which to define an operation.
+   *
+   * @return array
+   *   An array of operation definitions.
+   *
+   * @see hook_entity_operation()
+   */
+  public function entityOperation(EntityInterface $entity) {
+    $operations = [];
+    if ($entity instanceof StoreInterface && $this->currentUser->hasPermission('view own commerce_product')) {
+      $url = $entity->toUrl();
+      $route = 'view.commerce_multistore_administer_stores.commerce_multistore_products_page';
+      $route_parameters = $url->getRouteParameters();
+      $options = $url->getOptions();
+      $operations['multistore_products'] = [
+      'title' => $this->t('Products'),
+      'weight' => -100,
+      'url' => $url->fromRoute($route, $route_parameters, $options),
+      ];
+
+      return $operations;
+    }
+  }
+
+}
