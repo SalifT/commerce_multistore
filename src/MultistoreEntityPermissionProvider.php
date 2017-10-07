db02e5fa8b1f7617d8143a51ce841b704c0db41d
drugan <proshins@gmail.com>
2017-10-07 21:30:15 +0300

    Set MultistoreEntityPermissionProvider::class

---
+++ src/MultistoreEntityPermissionProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\commerce_multistore;
+
+use Drupal\commerce\EntityPermissionProvider;
+
+/**
+ * Provides Commerce Multistore entity permissions.
+ *
+ * @see \Drupal\entity\EntityPermissionProvider
+ */
+class MultistoreEntityPermissionProvider extends EntityPermissionProvider {}
